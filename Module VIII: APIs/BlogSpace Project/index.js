116c58f92267d763ff1e04662127a5ed7150d71e
Chris Waters <chriswatersaudio@gmail.com>
2024-01-05 22:19:29 -0500

    refactor --> added render function

--- Module VIII: APIs/BlogSpace Project/index.js
+++ Module VIII: APIs/BlogSpace Project/index.js
@@ -1,40 +1,33 @@
- /**
-     * Challenge: Send this off to the server!
-     * 
- 
-     * 3. method: ???
-     * 4. Request body: ??? (Remember to turn it into JSON)
-     * 5. Headers: ??? (Check the JSON Placeholder API docs or past casts for help)
-     */
-
 
 const postButtonEl = document.getElementById('post-button')
 const titleEl = document.getElementById("post-title")
 const bodyEl = document.getElementById("post-area")
 const postFormEl = document.getElementById("postInput")
+const postList = document.getElementById("blog-list")
+
+let postArr = []
+function renderPosts() {
+    let html = ""
+    for (let post of postsArr) {
+        html += `
+            <h3>${post.title}</h3>
+            <p>${post.body}</p>
+            <hr />
+         `
+    }
+    postList.innerHTML = html
+}
 
 
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     .then(res => res.json())
     .then(data => {
-        const postsArr = data.slice(0, 5)
-        let html = ""
-        for (let post of postsArr) {
-            html += `
-                <h3>${post.title}</h3>
-                <p>${post.body}</p>
-                <hr />
-            `
-        }
-        document.getElementById("blog-list").innerHTML = html
+        postsArr = data.slice(0, 5)
+        renderPosts()
     })
 
 
-    // postButtonEl.addEventListener('click', (event)=> {
-    //     event.preventDefault()
-    //     newPost = {title: titleEl.value, body: bodyEl.value}
-    //     postItem(newPost)
-    // })
+  
 
 postFormEl.addEventListener('submit', (event)=> {
     event.preventDefault()
